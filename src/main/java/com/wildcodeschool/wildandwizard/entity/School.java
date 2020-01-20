8c78a13752a5d3cc26761b6105f724246d0875c4
Michael Blisner <Michael.Blisner@telekom.de>
2020-01-20 12:59:52 +0100

    loesung

--- src/main/java/com/wildcodeschool/wildandwizard/entity/School.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/School.java
@@ -1,46 +1,54 @@
 package com.wildcodeschool.wildandwizard.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 // TODO : update this entity
 
+@Entity
 public class School {
 
-    private Long id;
-    private String name;
-    private Long capacity;
-    private String country;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private Long capacity;
+	private String country;
 
-    public School() {
-    }
+	public School() {
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public Long getCapacity() {
-        return capacity;
-    }
+	public Long getCapacity() {
+		return capacity;
+	}
 
-    public void setCapacity(Long capacity) {
-        this.capacity = capacity;
-    }
+	public void setCapacity(Long capacity) {
+		this.capacity = capacity;
+	}
 
-    public String getCountry() {
-        return country;
-    }
+	public String getCountry() {
+		return country;
+	}
 
-    public void setCountry(String country) {
-        this.country = country;
-    }
+	public void setCountry(String country) {
+		this.country = country;
+	}
 }
